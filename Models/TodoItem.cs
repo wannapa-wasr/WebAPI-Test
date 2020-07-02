fcd2de5037e4211d0d6252111bc9397cf66cb50d
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-07-02 17:04:57 +0700

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class TodoItem
+{
+	public long Id { get; set; }
+	public string Name { get; set; }
+	public bool IsComplete { get; set; }
+}
